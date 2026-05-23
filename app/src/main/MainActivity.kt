8e58d247cd91748d1f8af039be89d46c80a2d494
aurarewardsvibe-hub <aurarewardsvibe@gmail.com>
2026-05-23 12:04:25 +0530

    android app setup

---
+++ app/src/main/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.antibanmart.app
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val text = TextView(this)
+        text.text = "ANTIBAN MART 🔥"
+        text.textSize = 28f
+
+        setContentView(text)
+    }
+}
